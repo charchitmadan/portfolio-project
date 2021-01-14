37e6113c706b6a7f9ac6a9b4ecda29153c4401e5
Charcit Madan -X (chamadan - WIPRO LIMITED at Cisco) <chamadan@partnet.cisco.com>
2021-01-14 15:05:07 +0530

    Blog Functionality and Polish

--- blogs/models.py
+++ blogs/models.py
@@ -6,3 +6,12 @@ class Blog(models.Model) :
     pub_date = models.DateTimeField()
     body = models.TextField()         #Used TextField instead of CharField because TextField allows to enter more text. We can use CharField also.
     image = models.ImageField(upload_to = "images/")
+
+    def summary(self) :
+        return self.body[:100]
+
+    def pretty_time(self) :
+        return self.pub_date.strftime('%b %e %Y')
+
+    def __str__(self) :
+        return self.title
